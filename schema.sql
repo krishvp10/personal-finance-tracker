94a245244172643948a2387b3f9c811d11021dd8
KrishPatel <krishvp10@gmail.com>
2026-05-31 14:12:41 +0530

    tables in the database has been created and schema.sql is created for the reference purpose

---
+++ schema.sql
@@ -0,0 +1,14 @@
+ CREATE TABLE transactions (
+    ID SERIAL PRIMARY KEY,
+    date DATE NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    type VARCHAR(10) NOT NULL CHECK (type IN ('income', 'expense')),
+    category VARCHAR(20) NOT NULL,
+    description VARCHAR(50) NOT NULL
+ );
+
+ CREATE TABLE categories (
+    ID SERIAL PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    type VARCHAR(10) NOT NULL CHECK (type IN ('income', 'expense'))
+ );
